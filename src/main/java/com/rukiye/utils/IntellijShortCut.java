3a95097503aaefdd1658c5baa797c14fc790794c
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-07-28 01:53:59 +0300

    Kısayollar

---
+++ src/main/java/com/rukiye/utils/IntellijShortCut.java
@@ -0,0 +1,9 @@
+package com.rukiye.utils;
+
+public class IntellijShortCut {
+    /*Setting ==>ctrl +alt+s
+    Alt +Ins ==>constructor,toString,getter ,setter
+    ctrl +/ =>single comment
+    strl +shift +/ =>multiple comment
+*/
+}
